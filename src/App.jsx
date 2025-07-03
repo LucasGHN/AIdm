7e6342f681b310e925c2025fdd0da9b5ab684f30
关昊楠 <364257511@qq.com>
2025-07-03 21:41:31 +0800

    初始化项目

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ minHeight: '100vh', background: '#81D8D0' }}>
+      <h1>Hello Vite + React!</h1>
+    </div>
+  );
+} 
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// 这是一个标准的函数式组件，命名为 HelloWorld
+const HelloWorld: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>欢迎使用 React 组件！</p>
+    </div>
+  );
+};
+
+export default HelloWorld; 
